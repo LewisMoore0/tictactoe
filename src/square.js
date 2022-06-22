3c8c7ea35eafb398d6567b379d157f25c4ae8079
Lewis Moore <lewismooredev@gmail.com>
2022-06-22 18:22:38 +0100

    Pass state in to square props

--- src/square.js
+++ src/square.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class Square extends React.Component {
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        };
+    }
+    render() { 
       return (
-        <button className="square">
+        <button className="square" onClick={ () => { console.log('click') }}>
           {this.props.value}
         </button>
       );
